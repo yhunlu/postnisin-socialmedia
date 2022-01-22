f4037bebb81c782f1defe0f12a5ec0e228de253b
mrsbluegreen <mrsbluegreen@hotmail.com>
2022-01-22 22:44:09 +0300

    added pin component, initial setup for create pin

--- postnisin_frontend/src/components/CreatePin.jsx
+++ postnisin_frontend/src/components/CreatePin.jsx
@@ -1,6 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { AiOutlineCloudUpload } from 'react-icons/ai'
+import { MdDelete } from 'react-icons/md'
+import { useNavigate } from 'react-router-dom'
 
-const CreatePin = () => {
+import { Spinner } from '.'
+import { client } from '../client'
+import { categories } from '../utils/data'
+
+const CreatePin = ({ user }) => {
+    const [title, setTitle] = useState('');
+    const [about, setAbout] = useState('');
+    const [destination, setDestination] = useState('');
+    const [loading, setLoading] = useState(false);
+    const [fields, setFields] = useState(null);
+    const [category, setCategory] = useState(null);
+    const [imageAsset, setImageAsset] = useState(null);
+    const [wrongImageType, setWrongImageType] = useState(false)
+
+    const navigate = useNavigate();
+    
     return (
         <div>
             CreatePin
--- postnisin_frontend/src/container/Home.jsx
+++ postnisin_frontend/src/container/Home.jsx
@@ -8,16 +8,14 @@ import { userQuery } from '../utils/data';
 import { client } from '../client';
 import Pins from './Pins';
 import logo from '../assets/logo.png';
+import { fetchUser } from '../utils/fetchUser';
 
 const Home = () => {
   const [toggleSidebar, setToggleSidebar] = useState(false);
   const [user, setUser] = useState();
   const scrollRef = useRef(null);
 
-  const userInfo =
-    localStorage.getItem('user') !== 'undefined'
-      ? JSON.parse(localStorage.getItem('user'))
-      : localStorage.clear();
+  const userInfo = fetchUser();
 
   useEffect(() => {
     const query = userQuery(userInfo?.googleId);
